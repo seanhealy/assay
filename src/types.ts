1ab9866907fc55e4736d551f79519d925a1b0da8
Sean Healy <s@xib.ca>
2026-04-04 02:19:54 -0600

    chore: newline

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,7 @@
 export interface LiquidPresetOptions {
 	/** Paths to directories containing .liquid template files */
 	liquidPaths?: string[];
+
 	/** Path to the theme assets directory. Defaults to './assets' */
 	assetsPath?: string;
 }
